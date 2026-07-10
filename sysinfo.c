763f98ee2ad96a357a6fdb72e80329259b75c319
WilGulf <wilgot.ulfstedt@outlook.com>
2026-07-10 17:06:46 +0300

    Added temporary values for all modules

---
+++ sysinfo.c
@@ -0,0 +1,44 @@
+#include "sys/sysctl.h"
+#include "sysinfo.h"
+#include "common.h"
+#include "string.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+const char *getCommandOutput(char *cmd[128])
+{
+
+    FILE *fp;
+    char path[1035];
+
+    fp = popen(cmd, "r");
+    if (fp == NULL)
+    {
+        return "";
+    }
+
+    while (fgets(path, sizeof(path), fp) != NULL) {}
+
+    pclose(fp);
+
+    char buffer[128];
+
+    int i = 0;
+    for (; i < strlen(path); i++) {
+        if (path[i] == '\n') {
+            break;
+        }
+        buffer[i] = path[i];
+    }
+    
+
+    return path;
+}
+
+const char *getHostname() {
+    return "wilgulf@macbook";
+}
+
+const char *getKernel() {
+    return "darwin";
+}
---
+++ sysinfo.h
@@ -0,0 +1,30 @@
+#ifndef SYSINFO_H
+#define SYSINFO_H
+
+struct sysinfo {
+    char hostname[256];
+    char os[256];
+    char kernel[256];
+    int uptime;
+    int package;
+    char shell[256];
+    char display[256];
+    char terminal[256];
+    char cpu[256];
+    char gpu[256];
+    int memory_max;
+    int memory_use;
+    int swap_max;
+    int swap_use;
+    int disk_max;
+    int disk_use;
+    float local_ip;
+    int battery;
+    char locale[256];
+};
+
+
+const char* getHostname();
+const char* getKernel();
+
+#endif
